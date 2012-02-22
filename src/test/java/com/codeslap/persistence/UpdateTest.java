1ae2d6d1e3f46357cdf53a758130b36a572e9cf4
Cristian <cristian@elhacker.net>
2012-02-21 21:41:53 -0500

    - Added unit test for update

---
+++ src/test/java/com/codeslap/persistence/UpdateTest.java
@@ -0,0 +1,65 @@
+package com.codeslap.persistence;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author cristian
+ */
+public class UpdateTest extends SqliteTest {
+    @Test
+    public void updateTest() {
+        // let's insert a record
+        Random random = new Random();
+        ExampleAutoincrement foo = new ExampleAutoincrement();
+        foo.name = "Cristo Loco";
+        foo.number = random.nextInt();
+        foo.decimal = random.nextFloat();
+        foo.bool = random.nextBoolean();
+        getAdapter().store(foo);
+
+        // now, let's create a new object with different data
+        ExampleAutoincrement bar = new ExampleAutoincrement();
+        String name = bar.name = "Cristo Loco";
+        int number = bar.number = random.nextInt();
+        float decimal = bar.decimal = random.nextFloat();
+        boolean bool = bar.bool = random.nextBoolean();
+
+        // after updating this record, all its data should have changed...
+        getAdapter().update(bar, foo);
+        ExampleAutoincrement baz = getAdapter().findAll(ExampleAutoincrement.class).get(0);
+        assertEquals(name, baz.name);
+        assertEquals(number, baz.number);
+        assertEquals(decimal, baz.decimal, 0.0);
+        assertEquals(bool, baz.bool);
+    }
+    @Test
+    public void manualUpdateTest() {
+        // let's insert a record
+        Random random = new Random();
+        ExampleAutoincrement foo = new ExampleAutoincrement();
+        foo.name = "Cristo Loco";
+        foo.number = random.nextInt();
+        foo.decimal = random.nextFloat();
+        foo.bool = random.nextBoolean();
+        getAdapter().store(foo);
+
+        // now, let's create a new object with different data
+        ExampleAutoincrement bar = new ExampleAutoincrement();
+        String name = bar.name = "Cristo Loco";
+        int number = bar.number = random.nextInt();
+        float decimal = bar.decimal = random.nextFloat();
+        boolean bool = bar.bool = random.nextBoolean();
+
+        // after updating this record, all its data should have changed...
+        getAdapter().update(bar, "name LIKE ?", new String[]{foo.name});
+        ExampleAutoincrement baz = getAdapter().findAll(ExampleAutoincrement.class).get(0);
+        assertEquals(name, baz.name);
+        assertEquals(number, baz.number);
+        assertEquals(decimal, baz.decimal, 0.0);
+        assertEquals(bool, baz.bool);
+    }
+}
